9a817e1d88a80714739cce1b098b7dfd7a6ed014
daviddarx <daviddarx@gmail.com>
2021-05-11 22:38:44 +0200

    feat(navigation) adds secondary links style

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -8,16 +8,16 @@
       class="navigation__container"
       v-if="this.isDisplayed"
     >
-      <router-link
+      <a href="mailto:hallo@cedric-knapp.ch" class="navigation__link navigation__link--secondary navigation__email">hallo@cedric-knapp.ch</a><!--
+      --><a href="tel:+41 77 425 60 66" class="navigation__link navigation__link--secondary navigation__phone">+41 77 425 60 66</a><!--
+      --><router-link
         class="navigation__link"
         to="/projekte"
       >Projekte</router-link><!--
       --><router-link
         class="navigation__link"
         to="/ueber-mich"
-      >Über mich</router-link><!--
-      --><a href="mailto:hallo@cedric-knapp.ch" class="navigation__link navigation__email">hallo@cedric-knapp.ch</a><!--
-      --><a href="tel:+41 77 425 60 66" class="navigation__phone navigation__link">+41 77 425 60 66</a>
+      >Über mich</router-link>
     </div>
   </nav>
 </template>
