b1e697ad5837e4953e343c52aea35265cc876f3d
daviddarx <daviddarx@gmail.com>
2021-05-13 09:53:50 +0200

    feat(footer) implements classes for text-links

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -2,11 +2,11 @@
 <template>
   <div class="footer">
     <p class="footer__copyright">
-      ©{{this.year}} Cédric Knapp <span class="footer__dash">–</span> <a href="mailto:hallo@cedric-knapp.ch">hallo@cedric-knapp.ch</a> <span class="footer__dash">–</span> <a href="tel:+41 77 425 60 66">+41 77 425 60 66</a>
+      ©{{this.year}} Cédric Knapp <span class="footer__dash">–</span> <a href="mailto:hallo@cedric-knapp.ch" class="text-link">hallo@cedric-knapp.ch</a> <span class="footer__dash">–</span> <a href="tel:+41 77 425 60 66" class="text-link">+41 77 425 60 66</a>
 
     </p>
     <p class="footer__credits">
-      Design + Code : <a href="https://www.daviddarx.com" target="_blank">David Darx</a>
+      Design + Code : <a href="https://www.daviddarx.com" target="_blank" class="text-link">David Darx</a>
     </p>
   </div>
 </template>
