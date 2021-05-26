9bacbb1ec4a7ad119012ce06adc9b63aadba91c0
daviddarx <daviddarx@gmail.com>
2021-05-26 21:09:23 +0200

    feat(contents) adds transitions content

--- src/contents.js
+++ src/contents.js
@@ -1,5 +1,6 @@
 import * as home from '../content/home/home.json';
 import * as about from '../content/about/about.json';
+import * as transitions from '../content/transitions/transitions.json';
 import * as projects from '../content/projects/projects.json';
 import * as projectsList from '../content/projects/projects/*.json';
 delete projectsList.default;
@@ -7,6 +8,7 @@ delete projectsList.default;
 export default {
   home: home,
   about: about,
+  transitions: transitions,
   projects: projects,
   projectsList: Object.keys(projectsList).map(objKey => projectsList[objKey]).sort((e1, e2) =>
     new Date(e2.date) - new Date(e1.date)
